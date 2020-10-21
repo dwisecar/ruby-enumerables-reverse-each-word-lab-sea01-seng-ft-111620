21d1c72390bc1ce77f6741b58f55b3c3cbf95f56
David Wisecarver <dwisecar@gmail.com>
2020-10-21 18:04:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
-  new_array.collect do |element|
-    element.reverse;
-  end
+  new_array.collect {|element| element.reverse }
+  
   new_array.to_s;
 end
